698def80bf8d6b877edffe3cb1bc37f2ff136879
Begginer617 <skorupskimilosz@gmail.com>
2023-09-24 21:12:46 +0200

    AAA

--- src/POP/Operacje.java
+++ src/POP/Operacje.java
@@ -1,9 +1,12 @@
+package POP;
+
 public class Operacje {
 
 
     public static void main(String[] args) {
         int a=10;
         int b=20;
+        int c=30;
         System.out.println(sumAsia(a,b));
         System.out.println();
     }
