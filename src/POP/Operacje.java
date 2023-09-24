e092e79cd17d252edda6490c8576309d7fea0cd0
Begginer617 <skorupskimilosz@gmail.com>
2023-09-24 21:07:47 +0200

    mr jones

--- src/POP/Operacje.java
+++ src/POP/Operacje.java
@@ -1,10 +1,13 @@
 public class Operacje {
 
+
     public static void main(String[] args) {
         int a=10;
         int b=20;
         System.out.println(sumAsia(a,b));
+        System.out.println();
     }
+
 
     public static int sumAsia(int a, int b) {
         return (a+b);
